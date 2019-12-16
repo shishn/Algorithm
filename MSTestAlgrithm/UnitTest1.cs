5145908b36aabb5719275385677cadaeebee2496
shishn <shishn@163.com>
2019-12-16 17:14:24 +0800

    完成阶乘、斐波那契、解密qq、判断回文和寻找完美数的单元测试

--- MSTestAlgrithm/UnitTest1.cs
+++ MSTestAlgrithm/UnitTest1.cs
@@ -1,3 +1,4 @@
+using System.Runtime.Intrinsics.X86;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Demo;
 namespace MSTestAlgrithm
@@ -11,6 +12,32 @@ namespace MSTestAlgrithm
         {
             int i=3;
             Assert.AreEqual(exercise.Factrial(i),6);
+        }
+        [TestMethod]
+        public void TestFabonacci()
+        {
+            Assert.AreEqual(exercise.Fabonacci(5),8);
+        }
+        [TestMethod]
+        public void TestDecryptQQ()
+        {
+            Assert.AreEqual(exercise.DecryptQQ("44724302"),"47404322");
+        }
+
+        [TestMethod]
+        public void TestPalindrome()
+        {
+            Assert.IsTrue(exercise.IsPalindrome("abcdcba"));
+            Assert.IsFalse(exercise.IsPalindrome("abcde"));
+        }
+        [TestMethod]
+        public void TestPerfectNum()
+        {
+            var res=exercise.FindPerfectNumber(1000);
+            Assert.AreEqual(res.Count,3);
+            Assert.AreEqual(res[0],6);
+            Assert.AreEqual(res[1],28);
+            Assert.AreEqual(res[2],496);
         }
     }
 }
